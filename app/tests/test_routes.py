9e4f0b8895d210472660031ad8a12736e942ec11
Antara P <63465565+antarapal235@users.noreply.github.com>
2022-11-01 15:45:50 -0400

    Testing that the model is being retrieved properly
    currently failing

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,22 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+def test_base_route():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/'
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.get_data() == b'try the predict route it is great!'
+
+def test_model_retrieved():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/model'
+    response = client.get(url)
+    assert response.status_code == 200
