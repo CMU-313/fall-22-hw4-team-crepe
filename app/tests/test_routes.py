910c7deae11592a4dc3846a62964961dd7349026
Shakthi Vignesh Angou <shakthiangou@shakthismacbook.wifi.local.cmu.edu>
2022-11-01 16:45:15 -0400

    My test case checks that the length of the output array is 3 and that all the elements in the array string.

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -13,3 +13,27 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+
+def test_output_types():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/model'
+    
+    data = {
+        "student_id": 0,
+        "failures": 0,
+        "schoolsup": True,
+        "activities": True,
+        "internet": True,
+        "studytime": 0,
+        "school": "CMU",
+        "age": 0
+    }
+
+    response = client.post(url,data = data)
+    
+    assert all(isinstance(elem, str) for elem in response)
+    assert len(response) == 3
+    assert response.status_code == 200
