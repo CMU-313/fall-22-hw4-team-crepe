cebe418375ee4637dfe2a34878c0287e5a81977b
Kaylin Li <53351309+kaylinli@users.noreply.github.com>
2022-11-08 23:04:28 -0500

    Add updated model, API, tests, README  (#5)
    * My test case checks that the length of the output array is 3 and that all the elements in the array string.
    
    * Implement test for post model 422 wrong type
    
    * Added test for missing argument
    
    * Check correct output type of get request
    
    * Modified notebook to generate new model
    
    * Implement post /model route
    
    * Refactored post /model and check for type error
    
    * added get model test
    
    * Fixed bugs in api unit tests
    
    * Attempting to fix unable to open pickle file error
    
    * second attempt to fix get model error
    
    * final attempt to fix model path
    
    * Fix test for get /model and post /model api
    
    * Fix model prediction
    
    * Generated new model with true accuracy with split testing data
    
    * Update model_build.ipynb
    
    * Updated README to include information about the ML model
    
    * Update README.md
    
    Added features
    
    * Update README.md
    
    Added title
    
    * Clean up jupyter file
    
    Co-authored-by: Shakthi Vignesh Angou <shakthiangou@shakthismacbook.wifi.local.cmu.edu>
    Co-authored-by: Kevin Wu <kevinwu2@andrew.cmu.edu>
    Co-authored-by: Jason Perez <jasonper@andrew.cmu.edu>
    Co-authored-by: Kaylin Li <kaylinli@kleeshp>
    Co-authored-by: Antara P <63465565+antarapal235@users.noreply.github.com>
    Co-authored-by: ShakthiAngou <75957659+ShakthiAngou@users.noreply.github.com>

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -1,4 +1,6 @@
+import pickle
 import this
+import json
 from flask import Flask, jsonify, request
 import joblib
 import pandas as pd
@@ -15,19 +17,45 @@ def configure_routes(app):
     def hello():
         return "try the predict route it is great!"
 
+    @app.route('/model', methods = ['GET'])
+    def getModel():
+        with open(model_path, "rb") as pickle_file:
+            data = pickle_file.read()
+            response = app.make_response(data) 
+            response.content_type = "application/octet-stream"
+            return response 
 
-    @app.route('/predict')
+    @app.route('/model', methods = ['POST'])
     def predict():
-        #use entries from the query string here but could also use json
-        age = request.args.get('age')
-        absences = request.args.get('absences')
-        health = request.args.get('health')
-        data = [[age], [health], [absences]]
-        query_df = pd.DataFrame({
-            'age': pd.Series(age),
-            'health': pd.Series(health),
-            'absences': pd.Series(absences)
-        })
+        data = json.loads(request.json)
+        # Expected Keys
+        predict_schema = {
+            "failures": int, 
+            "schoolsup": bool, 
+            "internet": bool,
+            "studytime": float,
+            "absences": int,
+            "Medu": int,
+            "Fedu": int,
+            "paid": bool,
+            "famsup": bool
+        }
+        try:
+            predict_dict = {k:data[k] for k in predict_schema}
+            for k in predict_schema:
+                if not isinstance(predict_dict[k], predict_schema[k]): raise TypeError
+        except (KeyError, TypeError):
+            return "Unprocessable Entity", 422
+        # Postprocessing
+        predict_dict["schoolsup"] = int(predict_dict["schoolsup"])
+        predict_dict["paid"] = int(predict_dict["schoolsup"])
+        predict_dict["internet"]=int(predict_dict["internet"])
+        predict_dict["famsup"]=int(predict_dict["famsup"])
+         
+        query_df = pd.DataFrame({k:pd.Series(v) for k,v in predict_dict.items()})
         query = pd.get_dummies(query_df)
+        print(query)
         prediction = clf.predict(query)
-        return jsonify(np.asscalar(prediction))
+        response = app.make_response(str(prediction[0]))
+        response.content_type = "html/text; charset=utf-8"
+        return response 
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,18 +1,7 @@
+import json
 from flask import Flask
 
 from app.handlers.routes import configure_routes
-
-
-def test_base_route():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/'
-
-    response = client.get(url)
-
-    assert response.status_code == 200
-    assert response.get_data() == b'try the predict route it is great!'
 
 def test_base_route():
     app = Flask(__name__)
@@ -32,3 +21,64 @@ def test_model_retrieved():
     url = '/model'
     response = client.get(url)
     assert response.status_code == 200
+    assert isinstance(response.data, bytes)
+
+def test_output_types():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/model'
+    
+    data = json.dumps({
+        "failures": 0, 
+        "schoolsup": True, 
+        "internet": True,
+        "studytime": 0.0,
+        "absences": 0,
+        "Medu": 0,
+        "Fedu": 0,
+        "paid": True,
+        "famsup": True
+    })
+
+    response = client.post(url,json = data)
+    
+    assert response.status_code == 200
+    assert isinstance(response.data, bytes)
+
+def test_post_422():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/model'
+    
+    data = json.dumps({
+        "student_id": 0,
+        "failures": 0,
+        "schoolsup": True,
+        "activities": True,
+        "internet": True,
+        "studytime": 0,
+    })
+
+    response = client.post(url,json=data)
+    assert response.status_code == 422
+    
+def test_post_model_route_422_wrong_type():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/model'
+    data = json.dumps({
+        "student_id": 0,
+        "failures": 0,
+        "schoolsup": True,
+        # Wrong type 
+        "activities": 3,
+        "internet": True,
+        "studytime": 0,
+        "age": 0
+    })
+    
+    response = client.post(url, json=data)
+    assert response.status_code == 422 
